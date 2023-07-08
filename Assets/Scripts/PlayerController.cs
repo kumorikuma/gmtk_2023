52361f409c026294e3b1715b43253222c0197152
Bo Yin <nobiy.echoz@gmail.com>
2023-07-07 21:39:15 -0700

    Rotate player sprite on movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 /*Simple player movement controller, based on character controller component,
 with footstep system based on check the current texture of the component*/
 public class PlayerController : MonoBehaviour {
-    [NonNullField]
-    public Animator Animator;
+    [SerializeField]
+    private GameObject Sprite;
 
     [Header("Movement")]
     [SerializeField]
@@ -15,14 +15,17 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private float DashBurstSpeed = 5.0f;
 
-
-
     //Private movement variables
     private Vector2 inputMoveVector;
     private Rigidbody2D rb;
     private bool isDashing;
     private bool dashBurst;
     private bool isBiting;
+
+    // Movement animation
+    float lastRotationY = 0f;
+    float rotationSmooth = 5.0f;
+    float tiltAngle = 30.0f;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
@@ -44,6 +47,7 @@ public class PlayerController : MonoBehaviour {
     }
 
     private void Update() {
+        UpdateRotations();
     }
 
     private void FixedUpdate() {
@@ -67,5 +71,26 @@ public class PlayerController : MonoBehaviour {
             yAcceleration *= Speed;
         }
         rb.velocity = new Vector3(rb.velocity.x + xAcceleration, rb.velocity.y + yAcceleration, 0);
+    }
+
+    private void UpdateRotations() {
+        float rotationY = lastRotationY;
+        float rotationZ = 0f;
+        // Flip the player
+        if (inputMoveVector.x > 0.01) {
+            rotationY = 0f;
+        } else if (inputMoveVector.x < -0.01) {
+            rotationY = 180f;
+        }
+        // Keep the same facing if no button is pressed
+        lastRotationY = rotationY;
+
+        if (inputMoveVector.y > 0.01) {
+            rotationZ = tiltAngle;
+        } else if (inputMoveVector.y < -0.01) {
+            rotationZ = -tiltAngle;
+        }
+        Quaternion target = Quaternion.Euler(0f, rotationY, rotationZ);
+        Sprite.transform.rotation = Quaternion.Slerp(Sprite.transform.rotation, target,  Time.deltaTime * rotationSmooth);
     }
 }
