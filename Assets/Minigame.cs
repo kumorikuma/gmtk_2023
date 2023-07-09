a7e081001d6a62e37b073685502ae2d195467f9b
Francis Ge <francis.yunfeng.ge@gmail.com>
2023-07-08 22:50:45 -0700

    Added fishing minigame

---
+++ Assets/Minigame.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame : MonoBehaviour {
+    public GameObject FishIcon;
+    public GameObject FishermanBar;
+    public Transform TopOfBar;
+
+    private bool fishermanFollow = false;
+    private float followSpeed = 0.1f;
+    private float followDelay = 0.325f; // Roughly human reaction time
+    private float fishermanBarSize = 1.0f;
+
+    private struct TimePosition {
+        public float time;
+        public Vector3 position;
+
+        public TimePosition(float time, Vector3 position) {
+            this.time = time;
+            this.position = position;
+        }
+    }
+
+    private Queue<TimePosition> timePositions = new Queue<TimePosition>();
+
+    // Start is called before the first frame update
+    void Start() {
+        ShowMinigame(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!fishermanFollow) {
+            return;
+        }
+
+        // Oldest items in queue will also be oldest in time. They won't be consumed until they are at least older than the delay.
+        timePositions.Enqueue(new TimePosition(Time.time, FishIcon.transform.position));
+
+        // Remove elements from the queue until the front element's time is within the last 0.2 seconds
+        TimePosition timePosFromPast = new TimePosition(0, Vector3.zero);
+        while ((Time.time - timePositions.Peek().time) >= followDelay) {
+            timePosFromPast = timePositions.Dequeue();
+        }
+
+        if (timePosFromPast.time == 0) {
+            return;
+        }
+
+        float targetYPos = Mathf.Clamp(timePosFromPast.position.y, transform.position.y + fishermanBarSize / 2.0f, TopOfBar.transform.position.y - fishermanBarSize / 2.0f);
+        Vector3 targetPosition = new Vector3(timePosFromPast.position.x, targetYPos, timePosFromPast.position.z);
+        FishermanBar.transform.position = Vector3.Lerp(FishermanBar.transform.position, targetPosition, followSpeed);
+    }
+
+    public void ShowMinigame(bool isVisible) {
+        this.gameObject.SetActive(isVisible);
+        if (!isVisible) {
+            SetFishermanFollow(false);
+        }
+    }
+
+    public void SetFishermanFollow(bool shouldFollow) {
+        FishermanBar.SetActive(shouldFollow);
+        fishermanFollow = shouldFollow;
+    }
+
+    // public void SetFishermanPosition(float t) {
+    //     t = Mathf.Clamp(t, 0, 1);
+    //     FishermanBar.transform.position = Vector3.Lerp(transform.position, TopOfBar.transform.position, t);
+    // }
+
+    public void SetFishPosition(float t) {
+        t = Mathf.Clamp(t, 0, 1);
+        FishIcon.transform.position = Vector3.Lerp(transform.position, TopOfBar.transform.position, t);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -31,6 +31,8 @@ public class PlayerController : MonoBehaviour {
     }
     private bool isHoldingRope;
 
+
+
     // Movement animation
     float lastRotationY = 0f;
     float rotationSmooth = 5.0f;
@@ -42,6 +44,10 @@ public class PlayerController : MonoBehaviour {
     public RopeNode pinnedNode = null;
     [NonNullField]
     public FishermanAlert fishermanAlert = null;
+
+    [NonNullField]
+    public Minigame FishingMinigame = null;
+    private bool isInFishingMinigame = false;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
@@ -86,11 +92,14 @@ public class PlayerController : MonoBehaviour {
 
         // Alert the fisherman
         // TODO: Only do this if the gameplay state permits it
-        if (isBiting && pinnedNode != null) {
+        if (isBiting && pinnedNode != null && !isInFishingMinigame) {
             fishermanAlert.ShowAlert(true);
+            FishingMinigame.ShowMinigame(true);
             fishermanAlert.SetAlertness(0);
         } else if (!isBiting) {
+            isInFishingMinigame = false;
             fishermanAlert.ShowAlert(false);
+            FishingMinigame.ShowMinigame(false);
         }
 
         if (isBiting) {
@@ -140,6 +149,10 @@ public class PlayerController : MonoBehaviour {
             float ropeNormalSize = rope.GetRopeNormalSizeFromStartTo(pinnedNode);
             float ropeMaxStretch = 2.0f;
             float pctStretched = Mathf.Clamp((ropeStretch - 1.0f) / (ropeMaxStretch - 1.0f), 0, 1);
+
+            float ropeMaxStretchForMinigame = 3.0f;
+            float fishIconPosition = Mathf.Clamp((ropeStretch - 1.0f) / (ropeMaxStretchForMinigame - 1.0f), 0, 1);
+
             float ropeMaxSize = ropeMaxStretch * ropeNormalSize;
             Vector2 ropeAnchorPos2d = new Vector2(rope.GetFirstNode().transform.position.x, rope.GetFirstNode().transform.position.y);
             Vector2 ropeAnchorToPlayer = rb.position - ropeAnchorPos2d;
@@ -147,11 +160,30 @@ public class PlayerController : MonoBehaviour {
 
             Debug.Log("RopeStretch: " + ropeStretch);
             Debug.Log("DistanceToRopeAnchor: " + distanceToRopeAnchor);
-
+            Debug.Log("Pct Stetch: " + pctStretched);
             fishermanAlert.SetAlertness(pctStretched);
+            FishingMinigame.SetFishPosition(fishIconPosition);
 
             // Trigger the minigame
-            if (pctStretched > 1.0f) {
+            if (pctStretched >= 1.0f && !isInFishingMinigame) {
+                Debug.Log("Trigger Minigame");
+                isInFishingMinigame = true;
+
+                fishermanAlert.ShowAlert(false);
+                FishingMinigame.SetFishermanFollow(true);
+                // Let go of the line at the right time after the fisherman is tired to win!
+
+                // Dashing / pulling more will make the fish icon go higher
+                // Letting go will make the fish icon go lower
+                // Staying inside will make a red bar go up, if this goes up too high you let go automatically?
+
+                // The second fisherman will be better at the minigame.
+                // The third fisherman will be OP at the minigame, and force you to think of another way. Use the boot!
+
+                // Press left/right button repeatedly 
+            }
+
+            if (isInFishingMinigame) {
 
             }
 
